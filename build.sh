60e255560fa4606be4e18da660344d4b142ff488
panoskentros <panoskentros@gmail.com>
2025-09-24 19:24:34 +0300

    build fix

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+
+# Download dotnet-install script
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh -c 9.0.100 -InstallDir ./dotnet
+
+# Install .NET 9.0.305 SDK locally
+./dotnet-install.sh -Version 9.0.305 -InstallDir ./dotnet
+
+# Verify installation
 ./dotnet/dotnet --version
+
+# Publish your Blazor WASM app
 ./dotnet/dotnet publish -c Release -o output
