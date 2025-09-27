cd634088b900a45090c178e1ac164558e51fb0f6
panoskentros <panoskentros@gmail.com>
2025-09-27 16:45:47 +0300

    js timing fix

--- wwwroot/Js/GoogleSignIn.js
+++ wwwroot/Js/GoogleSignIn.js
@@ -3,23 +3,37 @@ let dotNetRef;
 window.initializeGoogleSignIn = function(dotnetHelper) {
     dotNetRef = dotnetHelper;
 
-    google.accounts.id.initialize({
-        client_id: "655071828601-0sna2oihnj5ieu52ud95t3buht58v4sh.apps.googleusercontent.com",
-        callback: handleCredentialResponse
-    });
+    function waitForGoogle() {
+        if (window.google && google.accounts && google.accounts.id) {
+            // Google library is ready
+            google.accounts.id.initialize({
+                client_id: "655071828601-0sna2oihnj5ieu52ud95t3buht58v4sh.apps.googleusercontent.com",
+                callback: handleCredentialResponse
+            });
 
-    function waitForButton() {
-        const buttonContainer = document.getElementById("googleSignInButton");
-        if (buttonContainer) {
-            google.accounts.id.renderButton(buttonContainer, { theme: "outline", size: "large",locale:"el-GR" });
+            function waitForButton() {
+                const buttonContainer = document.getElementById("googleSignInButton");
+                if (buttonContainer) {
+                    google.accounts.id.renderButton(buttonContainer, {
+                        theme: "outline",
+                        size: "large",
+                        locale: "el-GR"
+                    });
+                } else {
+                    setTimeout(waitForButton, 50);
+                }
+            }
+
+            waitForButton();
         } else {
             // Retry after 50ms
-            setTimeout(waitForButton, 50);
+            setTimeout(waitForGoogle, 50);
         }
     }
 
-    waitForButton();
+    waitForGoogle();
 };
+
 
 window.triggerGoogleSignIn = function() {
     google.accounts.id.prompt();
